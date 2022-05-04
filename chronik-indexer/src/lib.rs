dd69895614c6804c1f7935168b6228c8b22f148f
Tobias <ruck.tobias@gmail.com>
2022-05-04 10:43:36 +0200

    Add `Tokens` to `SlpIndexer` for `TokenStats` (#9)
    Fetches token stats by token_id, if the token exists.

--- chronik-indexer/src/lib.rs
+++ chronik-indexer/src/lib.rs
@@ -4,11 +4,13 @@ pub mod error;
 mod indexer;
 mod script_history;
 pub mod subscribers;
+mod tokens;
 mod txs;
 mod utxos;
 
 pub use crate::blocks::*;
 pub use crate::indexer::*;
 pub use crate::script_history::*;
+pub use crate::tokens::*;
 pub use crate::txs::*;
 pub use crate::utxos::*;
